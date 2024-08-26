f7d15b6c942981bf084b6f85b538506242b86f8f
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-08-25 21:54:34 -0600

    Up

--- models/stock_picking_report.py
+++ models/stock_picking_report.py
@@ -4,4 +4,5 @@ class StockPickingReport(models.Model):
     _inherit = 'stock.picking'
 
     def action_report_stock_picking_test(self):
+        # Enviar los documentos actuales al reporte
         return self.env.ref('custom_stock_report.action_report_stock_picking_test').report_action(self)
